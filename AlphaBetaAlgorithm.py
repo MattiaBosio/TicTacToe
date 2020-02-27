17b900abdc0ee7afa03721cdfb289d3680d34b86
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-02-27 17:28:27 +0100

    deleting useless class attribute turn, since already present in methods class

--- AlphaBetaAlgorithm.py
+++ AlphaBetaAlgorithm.py
@@ -3,12 +3,11 @@ from Game import Game
 
 class AlphaBetaAlgorithm:
 
-    def __init__(self, max_symbol, min_symbol, turn):
+    def __init__(self, max_symbol, min_symbol):
         self._max_symbol = max_symbol
         self._min_symbol = min_symbol
         self._game = None
-        self._turn = turn
-
+        
     def set_game(self, game: Game):
         self._game = game
 
